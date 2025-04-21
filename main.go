2b79c12ccfaee9d1ab62869a36f422fb9a23ef22
QwertyTux <62173670+QwertyTux@users.noreply.github.com>
2025-04-21 10:12:31 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import(
   "os"
   )
 
-//Главная функция
+//Main function
 func main()  {
 
   var p string
-  fmt.Print("Введите порт, который будет исользоваться: ")
+  fmt.Print("Write port in using programm: ")
   fmt.Fscan(os.Stdin, &p)
 
   http.HandleFunc("/", mainPage)
@@ -29,7 +29,7 @@ func main()  {
   }
 }
 
-//Функция главной страницы
+//Main page function
 func mainPage(w http.ResponseWriter, r *http.Request )  {
   tmpl, err := template.ParseFiles("templates/mainPage.html")
 
@@ -44,7 +44,7 @@ func mainPage(w http.ResponseWriter, r *http.Request )  {
   }
 }
 
-//Функция страницы 1
+//Page 1 function
 func  page_1(w http.ResponseWriter, r *http.Request )  {
   tmpl, err := template.ParseFiles("templates/page_1.html")
 
@@ -59,7 +59,7 @@ func  page_1(w http.ResponseWriter, r *http.Request )  {
   }
 }
 
-//Функция страницы 2
+//Page 2 function
 func  page_2(w http.ResponseWriter, r *http.Request )  {
   tmpl, err := template.ParseFiles("templates/page_2.html")
 
@@ -74,7 +74,7 @@ func  page_2(w http.ResponseWriter, r *http.Request )  {
   }
 }
 
-//Функция станицы 3
+//Page 3 function
 func page_3(w http.ResponseWriter, r *http.Request ){
 
   tmpl, err := template.ParseFiles("templates/page_3.html")
@@ -95,10 +95,10 @@ func page_3(w http.ResponseWriter, r *http.Request ){
   		log.Println(err)
   	}
 
-    vvod := r.PostFormValue("vvod")
+    inputs := r.PostFormValue("inputs")
 
     //alert
-    if vvod == "\"><script>alert(document.cookie)</script>" || vvod == "<script>alert(document.cookie)</script>" || vvod == "'><script>alert(document.cookie)</script>" {
+    if inputs == "\"><script>alert(document.cookie)</script>" || inputs == "<script>alert(document.cookie)</script>" || inputs == "'><script>alert(document.cookie)</script>" {
       tmpl, err := template.ParseFiles("templates/page_3_flug_alert.html")
       if err != nil{
         http.Error(w, err.Error(), 400)
@@ -112,7 +112,7 @@ func page_3(w http.ResponseWriter, r *http.Request ){
     }
 
     //document.write
-    if vvod == "\"><script>document.write(document.cookie)</script>" || vvod == "<script>document.write(document.cookie)</script>" || vvod == "'><script>document.write(document.cookie)</script>" {
+    if inputs == "\"><script>document.write(document.cookie)</script>" || inputs == "<script>document.write(document.cookie)</script>" || inputs == "'><script>document.write(document.cookie)</script>" {
       tmpl, err := template.ParseFiles("templates/page_3_flug_document.html")
       if err != nil{
         http.Error(w, err.Error(), 400)
@@ -126,7 +126,7 @@ func page_3(w http.ResponseWriter, r *http.Request ){
     }
     
     //console.log
-    if vvod == "\"><script>console.log(document.cookie)</script>" || vvod == "<script>console.log(document.cookie)</script>" || vvod == "'><script>console.log(document.cookie)</script>" {
+    if inputs == "\"><script>console.log(document.cookie)</script>" || inputs == "<script>console.log(document.cookie)</script>" || inputs == "'><script>console.log(document.cookie)</script>" {
       tmpl, err := template.ParseFiles("templates/page_3_flug_console.html")
       if err != nil{
         http.Error(w, err.Error(), 400)
